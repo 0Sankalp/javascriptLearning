d86a1a3fffd989913ef2bf456e1ca68011fff0f0
Sankalp Dubey <sankalpdubey0008@gmail.com>
2024-01-25 14:38:34 +0530

    There are many types of string method are there.I have done opertion on 8 of them,you will find here

---
+++ String/stringMethod.js
@@ -0,0 +1,23 @@
+let name ="Dwarika"
+console.log (name.length)
+// print the lenght of the strng
+
+console.log(name.toUpperCase())
+// print the string in uppercase "DWARIKA"
+
+console.log(name.toLowerCase())
+// print the string in uppercase "dwarika"
+
+console.log(name.slice(4,7))
+// it will print from 5 = i to 7 = a
+
+console.log(name.replace("Dwar","Radh"))
+// use for replacing the string
+
+let friend = "Ganesh"
+console.log(name.concat(" is a friend of ",friend," ok " ))
+//print Dwarika is a friend of Ganesh ok
+
+let friend2 = "      Radhika      "
+console.log(friend2.trim())
+//      Radhika      it willl reduce the space of the string
