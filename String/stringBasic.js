c3a8529e4ef1e98b5edb7a90ccec51caf6af93e1
Sankalp Dubey <sankalpdubey0008@gmail.com>
2024-01-25 14:32:20 +0530

    Here you will find some basic of strings

---
+++ String/stringBasic.js
@@ -0,0 +1,7 @@
+let name = 'Dwarika'
+console.log(name.length)
+// by this process you can print the lenght of the string
+
+let friend ="Ganesh"
+console.log (friend)
+// as you can see that you can us both single and double quotation for using the string but if you are using ("") this so you have to complete the string from this("") otherwise it will not work if you use this ('')instead of this("")
