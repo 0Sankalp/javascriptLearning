6179b7b649849a4d1f5161e45756857811a156b4
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-03-27 14:48:04 +0530

    table linlks

---
+++ DOM/tableLinks/script.js
@@ -0,0 +1,7 @@
+let t = document.body.firstElementChild.firstElementChild
+console.log(t.rows)
+console.log(t.caption)
+console.log(t.Head.firstElementChild)
+console.log(t.Foot)
+console.log(t.Bodies)
+console.log(t.rows[1].rowIndex)
