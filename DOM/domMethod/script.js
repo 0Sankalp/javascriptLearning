0320bf008029acb3398dd2fe218b339c5dd8be3d
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2024-03-28 02:32:45 +0530

    Searching the dom by these method

---
+++ DOM/domMethod/script.js
@@ -0,0 +1,9 @@
+// changing the colur of card title
+let ctitle = document.getElementById("firscardtitle")
+ctitle.style.color = "red"
+
+let ctitles = document.querySelectorAll(".card-title")
+ctitles[0].style.color = "red"
+ctitles[1].style.color = "blue"
+ctitles[2].style.color = "green"
+console.log(ctitles) 
